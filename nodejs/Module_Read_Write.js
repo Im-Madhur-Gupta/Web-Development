619d838684307246c2fb844ec98fe79a7e5d81f3
Madhur Gupta <202051112@iiitvadodara.ac.in>
2021-07-18 22:37:55 +0530

    jul 18

--- nodejs/Module_Read_Write.js
+++ nodejs/Module_Read_Write.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 let text = fs.readFileSync("./nodejs/read.txt","utf-8");
 console.log(text);
 
-// IMP - upar wali directory me ./ ke bad nodejs fir txt file ka name dalna pada kyoki ./ wo wali directory kholta he jaha maine terminal me jakar node script_name.js wala command dalke run karwaya tha, aur ye terminal ka command vs code ne Web-Development wala folder me khola tha to mujhe ./ ko batana pada ki Web-Developemnt wale me ni he uske andar nodejs folder me jao waha he.
+// IMP - upar wali directory me ./ ke bad nodejs fir txt file ka name dalna pada kyoki ./ wo wali directory kholta he jaha maine terminal me jakar node script_name.js; wala command dalke run karwaya tha, aur ye terminal ka command VS code ne Web-Development wala folder me khola tha to mujhe ./ ko batana pada ki Web-Developemnt wale me ni he uske andar nodejs folder me jao waha he.
 
 // readFileSync() will read our file synchronously, readFile() asynchronously read karta he.
 
@@ -13,4 +13,4 @@ text = text.concat("\n\t\t\t\t\t\t-Lucifer");
 // I have changed the text variable not read.txt.
 
 // Now writing our text variable to a new file write.txt synchronously
-fs.writeFileSync("./nodejs/write.txt",text);
+fs.writeFileSync("./nodejs/write.txt",text); //  agr file ni hogi to usse create kar dega
