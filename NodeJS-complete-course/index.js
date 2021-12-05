243e97ccb2ad18dea597e7ff2c567e28969da647
Madhur_Gupta <madleads2001@gmail.com>
2021-12-05 12:09:06 +0530

    misc removals

--- NodeJS-complete-course/index.js
+++
@@ -1,4 +0,0 @@
-// As JavaScript is universal we need to know where our code would be executing, is it in the browser or just in the backend.
-// Here, its just in the backend ie Nodejs, so things like alert, any visual popup that was supported natively by a browser is of no use in the backend, ie are NOT supported by Nodejs.
-
-// alert("hi"); --> No use in backend
