def5dac5011a13befe7b2c221a2e28340a30177c
madhur gupta <madhurgupta107@gmail.com>
2022-01-06 16:01:56 +0530

    code backup

---
+++ TailwindCSS/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1 className="text-blue-900 text-4xl font-bold underline">
+        Hello People, this is my first Tailwind CSS project.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ TailwindCSS/src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <nav className="px-6 py-4 bg-slate-600 text-cyan-50">
+      <ul className="flex">
+        <li className="px-2 cursor-pointer font-mono">Home</li>
+        <li className="px-2 cursor-pointer font-mono">About</li>
+        <li className="px-2 cursor-pointer font-mono">Contact Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
